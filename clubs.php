d13d18316f37efe714bd079749a27ab8b3b9b9c0
moue <hou@college.harvard.edu>
2014-10-18 13:41:51 -0400

    Fixed modals

--- clubs.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $title = "List of Clubs";
-  require('includes/common.php');
-  include 'includes/head.php';
-  include 'includes/header.php';
-  include 'includes/content/clubs_page.php';
-  include 'includes/footer.php';
-?>
-
